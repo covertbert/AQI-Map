ed95f090958412a2fc7273a2dc010b343f9c50c4
bertie.blackman <bertie.blackman@havas.com>
2017-03-08 12:16:08 +0000

    Refactoring, circle opacity based on weight

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,32 +18,53 @@ const getApiData = $.getJSON(apiQuery, (result) => {
 	});
 });
 
+function getHighestAQIValue() {
+	let highestAQIValue = 0;
+	placesArray.map((aqiData) => {
+		const currentAQIValue = aqiData.aqi;
+		if (currentAQIValue > highestAQIValue) {
+			highestAQIValue = currentAQIValue;
+		}
+		return true;
+	});
+	return highestAQIValue;
+}
+
+function setCircleOpacity(AQIValue) {
+	return parseInt(AQIValue, 10) / getHighestAQIValue();
+}
+
 function setGradient(aqi) {
-	const greenBlue = Math.floor(256 - ((aqi / 100) * 256));
+	const greenBlue = Math.floor(256 - ((aqi / getHighestAQIValue()) * 256));
 	return `rgb(256, ${greenBlue}, ${greenBlue})`;
 }
 
+function drawDataCircles(map) {
+	getApiData.then(() => {
+		placesArray.map((aqiData) => {
+			const circleCoordinates = { lat: aqiData.lat, lng: aqiData.lon };
+			if (!isNaN(aqiData.aqi)) {
+				const circle = new google.maps.Circle({
+					center: circleCoordinates,
+					strokeWeight: 0,
+					fillColor: setGradient(aqiData.aqi),
+					fillOpacity: setCircleOpacity(aqiData.aqi),
+					map,
+					radius: 20000,
+					label: aqiData.aqi.toString(),
+				});
+				return circle;
+			}
+			return true;
+		});
+	});
+}
+
 function initMap() {
-	const map = new google.maps.Map(document.getElementById('map'), {
+	const dataMap = new google.maps.Map(document.getElementById('map'), {
 		zoom: 6,
 		center: { lat: 54.0, lng: -3.0 },
 		mapTypeId: 'roadmap',
 	});
-
-	getApiData.then(() => {
-		placesArray.map((aqiData) => {
-			const coords = { lat: aqiData.lat, lng: aqiData.lon };
-			if (!isNaN(aqiData.aqi)) {
-				const circle = new google.maps.Circle({
-					center: coords,
-					strokeWeight: 0,
-					fillColor: setGradient(aqiData.aqi),
-					fillOpacity: 0.9,
-					map,
-					radius: 15000,
-					label: aqiData.aqi.toString(),
-				});
-			}
-		});
-	});
+	drawDataCircles(dataMap);
 }
